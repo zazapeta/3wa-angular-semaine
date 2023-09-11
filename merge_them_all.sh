eadd2200185a15d7aca728dd07ab16f278e2246c
zazapeta <7803873+zazapeta@users.noreply.github.com>
2023-09-11 15:05:47 +0200

    Add merge them all script

---
+++ merge_them_all.sh
@@ -0,0 +1,23 @@
+$branches = [];
+#!/bin/bash
+
+# Tableau contenant les noms des branches à fusionner
+branches=("Alexis" "Bryan" "nova" "sami" "Antoine" "fabien")
+
+# Nom de la branche cible
+branche_cible="main"
+
+# Se déplacer sur la branche cible
+git checkout $branche_cible
+
+# Mettre à jour la branche cible
+git pull origin $branche_cible
+
+# Boucle pour fusionner chaque branche
+for branche in "${branches[@]}"; do
+  # Fusionner la branche dans la branche cible
+  git merge "origin/$branche"
+done
+
+# Pousser les modifications fusionnées
+git push origin $branche_cible
