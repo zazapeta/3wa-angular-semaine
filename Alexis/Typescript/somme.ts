b3531cf8f43f021df06ebcb11791127f756a0265
Alexis Fromental <fromental.alexis@gmail.com>
2023-09-11 12:56:41 +0200

    Update somme.ts

--- Alexis/Typescript/somme.ts
+++ Alexis/Typescript/somme.ts
@@ -1,9 +1,22 @@
-let _value  = 0;
-let _value2 = 2;
-
 function somme(_value :number, _value2 :number) : number
 {
     return _value+_value2;
 }
 
-console.log(somme(_value,_value2));
+function main()
+{
+    let _value  = 0;
+    let _value2 = 2;
+
+    try
+    {
+        let _result = somme(_value,_value2);
+        console.log(_result);
+    }
+    catch(e)
+    {
+        console.error(e);
+    }
+}
+
+main();
