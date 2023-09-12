6eb8fe0f0173b074020c12ef569521f3917c53aa
zazapeta <7803873+zazapeta@users.noreply.github.com>
2023-09-12 17:03:49 +0200

    First pastries component

---
+++ sami/Angular/app-yams/src/app/pastries/pastries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pastries',
+  templateUrl: './pastries.component.html',
+  styleUrls: ['./pastries.component.scss']
+})
+export class PastriesComponent {
+
+}
