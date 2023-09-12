79cdfae3bdb24cc52524ebca62bc34590761410b
Alexis Fromental <fromental.alexis@gmail.com>
2023-09-12 10:29:48 +0200

    Commit add tdd for exercice 3

--- Alexis/Typescript/chap1_exercice3.ts
+++ Alexis/Typescript/chap1_exercice3.ts
@@ -28,8 +28,35 @@ function RandomGroup(_array :number[],_max:number) :Map<number,number[]>
 function case1()
 {
     const students : number[] = [12, 11, 123, 8, 9, 100, 90, 23, 22, 99, 198, 202, 11, 19, 78, 112] ;
+    const _fakeStudents : number[] = [12, 11, 123, 8, 9, 100, 90, 23, 22, 99, 198, 202, 11, 19, 78, 112] ;
     let _groups = RandomGroup(students,2);
     console.log(_groups);
+    let _count = 0;
+    let _result = 8;
+
+    _fakeStudents.forEach((student) =>
+    {
+        for(let i = 1;i<_groups.size;i++)
+        {
+            let _tmp = _groups.get(i);
+            if(_tmp)
+            {
+                for(let j = 0;j<_tmp.length;j++)
+                {
+                    if(_tmp[j] === student)
+                    {
+                        _count++;
+                    }
+                }
+            }
+
+        }
+    });
+
+    if(_groups.size === _result && _count === _fakeStudents.length)
+    {
+        console.log("Vous avez la bonne taille de groupe et tout les étudiants on était placer");
+    } else throw new Error("Vous avez pas la bonne taille de groupe ou tout les étudiants on pas était placer");
 }
 
 function main()
