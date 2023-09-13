391bf4569bc3cbab25d48fd1b93e663e1f5c5300
devSky <dev.ferrando@gmail.com>
2023-09-13 14:32:19 +0200

    template de l'application mis en oeuvre et fin du chap7

--- app-yams/src/app/app.module.ts
+++ app-yams/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PastriesComponent } from './pastries/pastries.component';
+import { PastrieDetailsComponent } from './pastrie-details/pastrie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PastriesComponent
+    PastriesComponent,
+    PastrieDetailsComponent
   ],
   imports: [
     BrowserModule,
