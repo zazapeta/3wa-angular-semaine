1e90d7a62939927d3f118523e9d0f97bd2a58dd4
zazapeta <7803873+zazapeta@users.noreply.github.com>
2023-09-14 12:39:23 +0200

    [WIP] Add oggle order on ingredients list

--- sami/Angular/app-yams/src/app/pastrie-details/pastrie-details.component.ts
+++ sami/Angular/app-yams/src/app/pastrie-details/pastrie-details.component.ts
@@ -2,13 +2,31 @@ import { Component, Input } from '@angular/core';
 
 import { Pastrie } from '../pastrie';
 
+enum ORDERS {
+  asc,
+  desc,
+}
 @Component({
   selector: 'app-pastrie-details',
   templateUrl: './pastrie-details.component.html',
-  styleUrls: ['./pastrie-details.component.scss']
+  styleUrls: ['./pastrie-details.component.scss'],
 })
 export class PastrieDetailsComponent {
   @Input() pastrie!: Pastrie;
   @Input() ingredients!: string[];
+  order: ORDERS = ORDERS.asc;
+  ORDERS = ORDERS;
+  toggleOrder(): void {
+    switch (this.order) {
+      case ORDERS.desc:
+        this.order = ORDERS.asc;
+        this.ingredients.sort().reverse();
+        break;
+      case ORDERS.asc:
+      default:
+        this.order = ORDERS.desc;
+        this.ingredients.sort();
+        break;
+    }
+  }
 }
-
